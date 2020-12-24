1e5bac3eef9c241ebd18f785a56c639a83904e47
Anirudh337675 <73932473+Anirudh337675@users.noreply.github.com>
2020-12-24 20:55:50 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,43 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        
+        var options={
+            isStatic:false,
+           
+        }
+        this.width=width;
+        this.height=height;
+        this.Visiblity = 255;
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        
+        World.add(world,this.body);
+    }
+        
+    display(){
+    //console.log(this.body.speed);
+   if(this.body.speed < 4){
+    var pos=this.body.position;
+    var angle=this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("lightblue");
+    strokeWeight(4)
+    stroke("blue")
+    rect(0,0, this.width,this.height,angle);
+    pop();
+   }
+   else{
+    World.remove(world, this.body);
+    push();
+    this.Visiblity = this.Visiblity - 5;
+    tint(255,this.Visiblity);
+    rect(0,0, this.width,this.height,angle);
+    pop();
+  }
+    
+    }
+}
---
+++ chain.js
@@ -0,0 +1,35 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+      
+        var c_options={
+            bodyA:bodyA,
+            pointB:pointB,
+            length:10,
+            stiffness:0.04
+        }
+        this.bodyA=bodyA
+        this.pointB=pointB
+        
+        this.sling=Constraint.create(c_options);
+        World.add(world,this.sling);
+    }
+    fly()
+      {
+          this.sling.bodyA=null;
+      }
+      display(){
+
+       
+        if(this.sling.bodyA){
+          var A = this.sling.bodyA.position;
+          var B = this.pointB;
+       
+
+        stroke ("blue")
+        strokeWeight(4)
+        line(A.x,A.y, B.x,B.y);
+        }
+      }
+      
+      
+    }
